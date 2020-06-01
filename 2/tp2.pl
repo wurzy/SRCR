ee0ea9c2fdb6eb6b39250dbb0bf4fc02d4e81f48
Válter <47797461+wurzy@users.noreply.github.com>
2020-06-01 18:14:39 +0100

    added a few extra predicates

--- 2/tp2.pl
+++ 2/tp2.pl
@@ -4,12 +4,34 @@
 :- set_prolog_flag( single_var_warnings,off ).
 :- set_prolog_flag( unknown,fail ).
 
-:- dynamic caminho/4.
+:- use_module(library(lists)).
 
 % criarCaminho(1,183,791,C).
+% carreirasAdjacentesCarreira(1,Lista); true.
+% paragem\([0-9]+, [0-9]+, 183,              183: 1,7,10,12,13,15
 
+% ------------------------------------------------ PREDICADOS AUXILIARES -------------------------------------------------------------
+%-----------------------------------------------------------------------------
+% Retorna a lista de carreiras a que uma paragem pertence.
+carreirasAdjacentes(Paragem,Lista):-
+    findall(Carreira, (paragem(Carreira,_,Paragem, _, _, _, _, _, _, _, _, _)),
+    Lista).
+
+%-----------------------------------------------------------------------------
+% Retorna a lista de carreiras adjacentes a uma determinada carreira.
+carreirasAdjacentesCarreiraAux([], []).
+
+carreirasAdjacentesCarreiraAux([Paragem|Paragens],[Lista1|Lista]):-
+    carreirasAdjacentes(Paragem,Lista1),
+    carreirasAdjacentesCarreiraAux(Paragens,Lista).
+
+carreirasAdjacentesCarreira(Carreira, Lista):-
+    findall(Paragem, (paragem(Carreira,_,Paragem, _, _, _, _, _, _, _, _, _)),Paragens),
+    carreirasAdjacentesCarreiraAux(Paragens,Lista).
+
+%-----------------------------------------------------------------------------
 % Retorna a lista de elementos um Inicio e um Fim na mesma carreira.
-criarCaminho(_,Fim,Fim,[Fim]).
+criarCaminho(_,Fim,Fim,[Fim]):- !.
 
 criarCaminho(Carreira,Inicio,Fim,[Inicio|Caminho]):-
     paragem(Carreira,Id1, Inicio, _, _, _, _, _, _, _, _, _),
@@ -17,8 +39,38 @@ criarCaminho(Carreira,Inicio,Fim,[Inicio|Caminho]):-
     paragem(Carreira,Id2, Adjacente, _, _, _, _, _, _, _, _, _),
     criarCaminho(Carreira, Adjacente, Fim, Caminho).
 
+%-----------------------------------------------------------------------------
+% Verifica se um elemento duma lista pertence a outra lista
+verificaMatriz([], _):- false.
+
+verificaMatriz([L1|_], Matriz):-
+    verificaLista(L1,Matriz), !.
+
+verificaMatriz([_|L2], Matriz):-
+    verificaMatriz(L2,Matriz).
+
+%-----------------------------------------------------------------------------
+% Verifica se um elemento duma matriz pertence a outra
+verificaMatriz([], _):- false.
+
+verificaMatriz([L1|_], Matriz):-
+    verificaLista(L1,Matriz), !.
+
+verificaMatriz([_|L2], Matriz):-
+    verificaMatriz(L2,Matriz).
+% ------------------------------------------------------ RESOLUÇÃO -------------------------------------------------------------------
+
+%-----------------------------------------------------------------------------
 % Predicado que cria um caminho entre duas paragens, se a carreira for igual.
 caminho(Inicio,Fim,_,Caminho):-
     paragem(Carreira,_, Inicio, _, _, _, _, _, _, _, _, _),
     paragem(Carreira,_ ,Fim, _, _, _, _, _, _, _, _, _),
-    criarCaminho(Carreira,Inicio,Fim,Caminho).
+    criarCaminho(Carreira,Inicio,Fim,Caminho),
+    !.
+%-----------------------------------------------------------------------------
+
+caminho(Inicio,Fim,Historico,Caminho):-
+    paragem(Carreira1,_, Inicio, _, _, _, _, _, _, _, _, _),
+    paragem(Carreira2,_ ,Fim, _, _, _, _, _, _, _, _, _),
+    carreirasAdjacentes(Carreira1,Adjs1),
+    carreirasAdjacentes(Carreira2,Adjs2)
